554a2133d9194cd064ef9d7b3edb3e7755bccf92
Sarah Ayanwale <sarahayanwale@gmail.com>
2022-06-07 09:46:23 +0200

    api folder added

---
+++ src/apis/profiles/model.js
@@ -0,0 +1,39 @@
+import mongoose from "mongoose"
+const { Schema, model } = mongoose
+
+const profileSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    surname: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    bio: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+      //server generated image url
+    },
+    area: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+)
+
+export default model("Profile", profileSchema)
