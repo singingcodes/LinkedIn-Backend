471dcc7a9e9b26573c97741dbde5e6c50bbf24d5
lbourke1234 <lbourke1234@gmail.com>
2022-06-07 10:47:24 +0100

    push

--- src/server.js
+++ src/server.js
@@ -1,20 +1,21 @@
-import express from "express"
-import cors from "cors"
-import mongoose from "mongoose"
-import listEndPoints from "express-list-endpoints"
+import express from 'express'
+import cors from 'cors'
+import mongoose from 'mongoose'
+import listEndPoints from 'express-list-endpoints'
 import {
   handleBadRequest,
   handleNotFound,
   handleUnauthorized,
-  genericServerError,
-} from "./errorHandlers.js"
+  genericServerError
+} from './errorHandlers.js'
+import experienceRouter from './apis/experiences/index.js'
 
 const server = express()
 
 server.use(cors())
 server.use(express.json())
 
-//Endpoint 1
+server.use('/experiences', experienceRouter)
 //Endpoint 2
 //Endpoint 3
 //Endpoint 4
@@ -28,8 +29,8 @@ server.use(genericServerError)
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING)
 
-mongoose.connection.on("connected", () => {
-  console.log("connected to mongo")
+mongoose.connection.on('connected', () => {
+  console.log('connected to mongo')
   server.listen(port, () => {
     console.table(listEndPoints(server))
     console.log(`server is running on port ${port}`)
