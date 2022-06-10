7eeb290dc9af0649e0c156d8c60258668ef65cbf
lbourke1234 <lbourke1234@gmail.com>
2022-06-10 11:44:38 +0100

    push

--- src/apis/likes/model.js
+++ src/apis/likes/model.js
@@ -5,7 +5,7 @@ const { Schema, model } = mongoose
 const likeSchema = new Schema(
   {
     user: [{ type: mongoose.Types.ObjectId, required: true, unique: true }],
-    post: { type: mongoose.Types.ObjectId, required: true, unique: true }
+    post: { type: mongoose.Types.ObjectId, required: true }
   },
   { timestamps: true }
 )
