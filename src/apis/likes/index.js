f57e39d21cc8ebad99e26d92fd780cd4abfd9559
lbourke1234 <lbourke1234@gmail.com>
2022-06-10 08:57:41 +0100

    push

--- src/apis/likes/index.js
+++ src/apis/likes/index.js
@@ -6,42 +6,4 @@ import createError from 'http-errors'
 
 const router = express.Router()
 
-router.post('/:postId/like', async (req, res, next) => {
-  try {
-    const post = PostModel.findById(req.params.postId)
-    if (!post) return next(createError(404, `Post with ID ${req.params.postId} not found!`))
-    const user = ProfileModel.findById(req.body.userId)
-    if (!user) return next(createError(404, `User with ID ${req.body.userId} not found!`))
-    const newLike = new LikeModel(req.body)
-    const { _id } = await newLike.save()
-    res.status(201).send(_id)
-  } catch (error) {
-    console.log(error)
-    next(error)
-  }
-})
-router.delete('/:postId/like/:likeId/:userId', async (req, res, next) => {
-  try {
-    const post = PostModel.findById(req.params.postId)
-    if (!post) return next(createError(404, `Post with ID ${req.params.postId} not found!`))
-
-    const user = ProfileModel.findById(req.body.userId)
-    if (!user) return next(createError(404, `User with ID ${req.body.userId} not found!`))
-
-    const like = LikeModel.findById(req.params.likeId)
-    if (!like) return next(createError(404, `Like with id ${req.params.likeId} not found!`))
-    const foundUser = like.user.find((user) => user._id === req.params.userId)
-    if (!foundUser) return next(createError(404, `User with id ${req.params.userId} not found!`))
-    const updatedLike = LikeModel.findByIdAndUpdate(
-      req.params.likeId,
-      { $pull: { 'like.user': foundUser } },
-      { new: true, runValidators: true }
-    )
-    res.status(204).send()
-  } catch (error) {
-    console.log(error)
-    next(error)
-  }
-})
-
 export default router
--- src/apis/posts/index.js
+++ src/apis/posts/index.js
@@ -1,10 +1,11 @@
 import express from 'express'
 import createError from 'http-errors'
-import postModel from './model.js'
+import PostModel from './model.js'
 import multer from 'multer'
 import { CloudinaryStorage } from 'multer-storage-cloudinary'
 import { v2 as cloudinary } from 'cloudinary'
-import profileModel from '../profiles/model.js'
+import ProfileModel from '../profiles/model.js'
+import LikeModel from '../likes/model.js'
 
 const postRouter = express.Router()
 
@@ -28,7 +29,7 @@ const cloudinaryUploader = multer({
 //GET /api/posts
 postRouter.get('/', async (req, res, next) => {
   try {
-    const posts = await postModel.find().populate({ path: 'user' })
+    const posts = await PostModel.find().populate({ path: 'user' })
     res.send(posts)
   } catch (err) {
     next(err)
@@ -37,7 +38,7 @@ postRouter.get('/', async (req, res, next) => {
 //GET /api/posts/:id
 postRouter.get('/:id', async (req, res, next) => {
   try {
-    const post = await postModel.findById(req.params.id).populate({ path: 'user' })
+    const post = await PostModel.findById(req.params.id).populate({ path: 'user' })
     if (!post) {
       next(createError(404, `Post with id ${req.params.id} not found`))
     }
@@ -49,7 +50,7 @@ postRouter.get('/:id', async (req, res, next) => {
 //POST /api/posts
 postRouter.post('/', async (req, res, next) => {
   try {
-    const newPost = new postModel(req.body)
+    const newPost = new PostModel(req.body)
     const { _id } = await newPost.save()
     res.status(201).send({ _id })
   } catch (err) {
@@ -59,7 +60,7 @@ postRouter.post('/', async (req, res, next) => {
 //PUT /api/posts/:id
 postRouter.put('/:id', async (req, res, next) => {
   try {
-    const post = await postModel.findByIdAndUpdate(req.params.id, req.body, {
+    const post = await PostModel.findByIdAndUpdate(req.params.id, req.body, {
       new: true,
       runValidators: true
     })
@@ -74,7 +75,7 @@ postRouter.put('/:id', async (req, res, next) => {
 //DELETE /api/posts/:id
 postRouter.delete('/:id', async (req, res, next) => {
   try {
-    const post = await postModel.findByIdAndDelete(req.params.id)
+    const post = await PostModel.findByIdAndDelete(req.params.id)
     if (!post) {
       next(createError(404, `Post with id ${req.params.id} not found`))
     }
@@ -86,7 +87,7 @@ postRouter.delete('/:id', async (req, res, next) => {
 
 postRouter.post('/:id/picture', cloudinaryUploader, async (req, res, next) => {
   try {
-    const post = await postModel.findByIdAndUpdate(
+    const post = await PostModel.findByIdAndUpdate(
       req.params.id,
       { image: req.file.path },
       {
@@ -100,6 +101,46 @@ postRouter.post('/:id/picture', cloudinaryUploader, async (req, res, next) => {
     res.send(post)
   } catch (err) {
     next(err)
+  }
+})
+
+// LIKES
+
+postRouter.post('/like/:postId', async (req, res, next) => {
+  try {
+    const post = PostModel.findById(req.params.postId)
+    if (!post) return next(createError(404, `Post with ID ${req.params.postId} not found!`))
+    const user = ProfileModel.findById(req.body.userId)
+    if (!user) return next(createError(404, `User with ID ${req.body.userId} not found!`))
+    const newLike = new LikeModel(req.body)
+    const { _id } = await newLike.save()
+    res.status(201).send(_id)
+  } catch (error) {
+    console.log(error)
+    next(error)
+  }
+})
+postRouter.delete('/like/:postId/:likeId/:userId', async (req, res, next) => {
+  try {
+    const post = PostModel.findById(req.params.postId)
+    if (!post) return next(createError(404, `Post with ID ${req.params.postId} not found!`))
+
+    const user = ProfileModel.findById(req.body.userId)
+    if (!user) return next(createError(404, `User with ID ${req.body.userId} not found!`))
+
+    const like = LikeModel.findById(req.params.likeId)
+    if (!like) return next(createError(404, `Like with id ${req.params.likeId} not found!`))
+    const foundUser = like.user.find((user) => user._id === req.params.userId)
+    if (!foundUser) return next(createError(404, `User with id ${req.params.userId} not found!`))
+    const updatedLike = LikeModel.findByIdAndUpdate(
+      req.params.likeId,
+      { $pull: { 'like.user': foundUser } },
+      { new: true, runValidators: true }
+    )
+    res.status(204).send()
+  } catch (error) {
+    console.log(error)
+    next(error)
   }
 })
 
